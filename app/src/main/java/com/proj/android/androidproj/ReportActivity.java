359171515d07bb38101219e8299a7eaa8a8cfebf
ronen amon <ronen@dodev.info>
2016-06-08 07:56:38 +0300

    8-6-2016

--- app/src/main/java/com/proj/android/androidproj/ReportActivity.java
+++ app/src/main/java/com/proj/android/androidproj/ReportActivity.java
@@ -20,24 +20,25 @@ public class ReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        TextView txtArea = (TextView)findViewById(R.id.AreaText);
-        EditText edtext = (EditText)findViewById(R.id.editText);
+        TextView ReportArea = (TextView)findViewById(R.id.ReportArea);
+        TextView ReporterName = (TextView)findViewById(R.id.ReporterName);
+        TextView ReporterPhone = (TextView)findViewById(R.id.ReporterPhone);
+        EditText edtext = (EditText)findViewById(R.id.editTextToReport);
 
         Bundle bund = getIntent().getExtras();
         int selectedItem = bund.getInt("AreaSelected");// get area from the bundle
         String Name = bund.getString("name");// get name from the bundle
         String Phone = bund.getString("phone");// get phone  from the bundle
+
+        ReportArea.setText( items[selectedItem].toString());
+        ReporterName.setText(Name);
+        ReporterPhone.setText(Phone);
+
         Log.d("name" , Name);
         Log.d("Phone", Phone);
         Log.d("selectedItem-1", String.valueOf(selectedItem));
         Log.d("selectedItem-2", items[selectedItem].toString());
-        Log.d("txtArea-1", String.valueOf(txtArea));
-        Log.d("txtArea-2", txtArea.getText().toString());
-        txtArea.setText(items[selectedItem]);
-        Log.d("txtArea-3", txtArea.getText().toString());
 
-        assert edtext != null;
-        edtext.setText("Area : " + items[selectedItem] + " " + "Name : " + Name + " Phone : " + Phone);
 
     }
 
